93d9160c82f93af19194065c646611be43d99007
Andres Atehortua <atehortua9620@gmail.com>
2022-05-03 17:57:09 -0500

    proyecto con nodemon

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express ();
+const path = require('path');
 
 app.listen(3000,()=>console.log('server corriendo'));
 
-app.get('/', function(req, res){
-    res.send('bienvenidos');
+app.use(express.static('Public'));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/Home.html'));
 })
 
