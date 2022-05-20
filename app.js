67a2a5cb924bc02da3217d533439a67736f8d737
Leodan2023 <leobellanz@gmail.com>
2022-05-19 23:44:58 -0300

    login

--- app.js
+++ app.js
@@ -22,3 +22,7 @@ app.get('/productCar', (req,res)=>{
 app.get('/register', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 })
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
