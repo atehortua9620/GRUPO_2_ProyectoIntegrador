370e6916d85af725992267fd29265a2362051499
Leodan2023 <leobellanz@gmail.com>
2022-05-26 23:39:42 -0300

    formulario crear y editar productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,6 +2,9 @@ const controller = {
     productDetail:(req,res)=>{
         res.render('./productDetail.ejs');
     },
+    createEditProduct: (req,res)=>{
+        res.render('./createEditProduct.ejs');
+    }
 }
 
 module.exports = controller;
