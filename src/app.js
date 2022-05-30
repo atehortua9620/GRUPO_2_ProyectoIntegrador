4fb5842b169198ba3b994975ad57a99d632385e6
Andres Atehortua <atehortua9620@gmail.com>
2022-05-29 20:50:36 -0500

    Se añadieron lineasen app para trabajar con metodos DELTE, PUT y POST, ademas de lineas para trabajar con JSON

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express ();
+const methodOverride = require('method-override');
 
 const mainRouter = require('./routers/mainRouters')
 const productRouter = require('./routers/productRouters')
@@ -10,9 +11,13 @@ app.set  ('views', './src/views');
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log('server corriendo en el puerto'+port));
 
+/* añadiendo lineas para trabajar con paquetes JSON */
+app.use(express.json());
+/*linea para trabajar con el metodo POST */
+app.use(express.urlencoded({extended: false}));
+/*Linea para trabajar con libreria method over ride (librerira para usar PUT y DELETE) */
+app.use(methodOverride('_method'));
 app.use(express.static('./src/Public'));
-
-
 app.use('/', mainRouter);
 app.use('/product', productRouter);
 
