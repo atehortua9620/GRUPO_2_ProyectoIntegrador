830c5eff2da0d875d9f4e3e0461ef044e5afce47
Andres Atehortua <atehortua9620@gmail.com>
2022-06-13 19:06:09 -0500

    Agregando funcionalidad añadir producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,9 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const  productos = JSON.parse(fs.readFileSync(path.join(__dirname, "../database/productos.json"),'utf-8'));
+
 const controller = {
     productDetail:(req,res)=>{
         res.render('./productDetail.ejs');
     },
     createEditProduct: (req,res)=>{
         res.render('./createEditProduct.ejs');
+        
+    },
+    procesandoData:(req, res)=>{
+
+        let id = productos.length + 1;
+        req.body.id = id;
+        const nuevo ={
+            ...req.body,
+        
+        }
+    
+        
+
+        res.redirect('/');
     }
 }
 
--- src/routers/productRouters.js
+++ src/routers/productRouters.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+const {createEditProduct,procesandoData } = require('../controllers/productController');
 
 const productController = require('../controllers/productController');
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, path.join(__dirname,'../public/images'));
+    },
+    filename: (req, file, cb)=>{
+        cb(null, Date.now() +'probando'+path.extname(file.originalname))
+    }
+})
+
+const uploadFile = multer({storage});
+
+
 router.get('/detail',productController.productDetail);
+
 router.get('/createEditProduct',productController.createEditProduct);
+router.post('/createEditProduct',uploadFile.any('image'), procesandoData);
 
 module.exports = router;
