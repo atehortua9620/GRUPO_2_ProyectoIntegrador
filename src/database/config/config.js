3c5e6cf7dcfe38ef43408c706a52bc4aec72b414
Andres Atehortua <atehortua9620@gmail.com>
2022-08-03 19:36:40 -0500

    organizacion files

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports= {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "modelo_DB",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
