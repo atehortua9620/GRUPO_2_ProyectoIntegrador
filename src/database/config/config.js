29642ceb570d5313016ba12d8d8e7bb05b8da0ed
Andres Atehortua <atehortua9620@gmail.com>
2022-08-03 19:53:57 -0500

    listo doc config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports= {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER || root,
+    "password": process.env.DB_PASSWORD || "",
+    "database": process.env.DB_NAME || "modeloDB",
+    "host": process.env.DB_HOST || "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
